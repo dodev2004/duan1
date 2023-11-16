2cc64aa310ac43209630a0d11897c32a1edf976a
Xuan Thang <thangijr@gmail.com>
2023-11-16 19:04:44 +0700

    update

---
+++ controller/user.php
@@ -0,0 +1,23 @@
+<?php
+    include "../view/user/header.php";
+    include "../view/user/footer.php";
+    // if(isset($_GET["act"]) && isset($_GET["page"])  && $_GET["act"]){
+    //     $act = $_GET["act"];
+    //     $page = $_GET["page"];
+    //    switch($page){
+    //         case "loaiphong":
+    //             if($act = "lk"){
+    //                 include "../view/admin/loaiphong/lk.php";
+    //             }
+    //             else if($act = "them") {
+    //                 include "../view/admin/loaiphong/add.php";
+    //             }
+    //             else {
+    //                 include "../view/admin/loaiphong/sua.php";
+    //             }
+    //         break;
+    //    }
+    // }
+    // include "../view/admin/footer.php";
+
+?>
---
+++ view/user/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="publicport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../public/base.css">
+    <link rel="stylesheet" href="../public/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../public/style.css">
+
+
+</head>
+
+<body>
+    <header>
+        <div class="wrapper">
+            <section class="header_bottom">
+                <section class="header_bottom-content">
+                    <section class="header_bottom-left">
+                        <nav>
+                            <li>
+                                <a class="logo" href="">Logo</a>
+                            </li>
+                            <li> <a href="">Trang chủ</a></li>
+                            <li>
+                                <a href="">Về chúng tôi</a>
+                            </li>
+                            <li class="list_room-item">
+                                <a class="menu_room" href="">Phòng <i class="fa-solid fa-angle-down"></i>
+
+                                </a>
+                                <nav class="todolist_room">
+                                    <a href="">Phòng đơn</a><a href="">Phòng khách</a><a href="">Phòng Vip</a>
+                                </nav>
+                            </li>
+                            <li> <a href="">Ẩm thực</a></li>
+                            <li> <a href="">Dịch vụ</a></li>
+                            <li> <a href="">Thư viện ảnh</a></li>
+                            <li><a href="">Liên hệ</a></li>
+
+                        </nav>
+                    </section>
+                    <nav>
+                        <a href="" class="header_top-user">
+                            Đăng ký
+                        </a>
+                        <a href="" class="header_top-user">
+                            Đăng nhập
+                        </a>
+                    </nav>
+                </section>
+
+
+            </section>
+        </div>
+    </header>
