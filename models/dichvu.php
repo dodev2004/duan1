df03ef4d816cfb7378122511c799ab2eedaa3dd5
Xuan Thang <thangijr@gmail.com>
2023-11-18 22:28:35 +0700

    update

--- models/dichvu.php
+++ models/dichvu.php
@@ -1,30 +1,33 @@
-<?php 
-    function db_dv_insert($name,$image){
-        $sql  = "INSERT INTO dichvu(name,icon) values(?,?)";
-        pdo_execute($sql,$name,$image);
-    }
-    function db_dv_select(){
-        $sql  = "SELECT * FROM dichvu";
-        return pdo_query($sql);
-    }
-    function db_dv_select_by_id($id){
-       $sql = "SELECT * from dichvu where id = ?";
-        return pdo_query_one($sql,$id);
-    }
-    function db_dv_delete($id){
-        if(is_array($id)){
-            foreach($id as $id_dv){
-                $sql = "DELETE FROM dichvu where id = ?";
-                pdo_execute($sql,$id_dv);
-            }
+<?php
+function db_dv_insert($name, $image)
+{
+    $sql  = "INSERT INTO dichvu(name,icon) values(?,?)";
+    pdo_execute($sql, $name, $image);
+}
+function db_dv_select()
+{
+    $sql  = "SELECT * FROM dichvu";
+    return pdo_query($sql);
+}
+function db_dv_select_by_id($id)
+{
+    $sql = "SELECT * from dichvu where id = ?";
+    return pdo_query_one($sql, $id);
+}
+function db_dv_delete($id)
+{
+    if (is_array($id)) {
+        foreach ($id as $id_dv) {
+            $sql = "DELETE FROM dichvu where id = ?";
+            pdo_execute($sql, $id_dv);
         }
-        else {
-            $sql = "DELETE FROM dichvu WHERE id = ?";
-            pdo_execute($sql,$id);
-        }
+    } else {
+        $sql = "DELETE FROM dichvu WHERE id = ?";
+        pdo_execute($sql, $id);
     }
-    function db_dv_update($id,$image,$name){
-        $sql = "UPDATE dichvu SET name = ?, icon =? where id = ?";
-        pdo_execute($sql,$name,$image,$id);
-    }
-?>
+}
+function db_dv_update($id, $image, $name)
+{
+    $sql = "UPDATE dichvu SET name = ?, icon =? where id = ?";
+    pdo_execute($sql, $name, $image, $id);
+}
