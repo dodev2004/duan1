e101c557e02f97109a49e68e25593dcad75d056e
Xuan Thang <thangijr@gmail.com>
2023-11-22 16:44:12 +0700

    update header

--- view/user/header.php
+++ view/user/header.php
@@ -7,18 +7,13 @@
     <title>Trang chủ</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../public/base.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/style1.css">
     <link rel="stylesheet" href="../public/style.css">
 
@@ -55,13 +50,27 @@
                         </nav>
                     </section>
                     <nav>
-                        <li class="list_room-item">
+                        <!-- <li class="list_room-item">
                             Tài khoản
                             <nav class="todolist_room">
                                 <a class="user_infomation-list" href="">Phòng đơn</a><a class="user_infomation-list"
                                     href="">Phòng khách</a><a class="user_infomation-list" href="">Phòng Vip</a>
                             </nav>
-                        </li>
+                        </li> -->
+                        <nav class="user_box">
+                            <?php if (!isset($_SESSION["user"])) { ?>
+                                <a href="?act=dangnhap" class="header_top-user">
+                                    Đăng nhập
+                                </a>
+                            <?php } else { ?>
+                                <li class="list_room-item">
+                                    <a class="menu_room color_black" href=""> <i class="fa-solid fa-user icon_user"></i></a>
+                                    <nav class="todolist_room">
+                                        <a href="?act=dangxuat">Đăng xuất</a>
+                                    </nav>
+                                </li>
+                            <?php } ?>
+                        </nav>
                     </nav>
                 </section>
 
